0f1b6b7ea216115b046c9953b4583492571557b3
tracymu <tracy.musung@moomumedia.com>
2015-01-31 13:00:08 +1100

    Seed Data

---
+++ db/seeds.rb
@@ -0,0 +1,51 @@
+Activity.delete_all
+Activity.create(
+  :title => 'Write a Letter of Support to an Asylum Seeker', 
+  :description => 'An initiative by Australian barrister Julian Barnside. A similar letter-writing campaign in the early 2000s was very valuable in helping keep up the spirits of asylum seekers. For more information <a href="http://www.julianburnside.com.au/letters2.htm">http://www.julianburnside.com.au/letters2.htm</a>', 
+  :estimated_time => '30 Mins', 
+  :estimated_cost => 0, 
+  :location => 'Anywhere')
+Activity.create(
+  :title => 'Help 3 People with 1 Blood Donation', 
+  :description => 'Modern processing techniques mean that a single blood donation, when separated into its components, can help at least three different patients. Currently only 1 in 30 people give blood, but 1 in 3 people will need blood in their lifetime. Blood donations are always needed and can literally help save a life. Find out how to donate at <a href="http://www.donateblood.com.au">www.donateblood.com.au</a>', 
+  :estimated_time => '1 Hour', 
+  :estimated_cost => 0, 
+  :location => 'Various Locations')
+Activity.create(
+  :title => 'Swap a Meal to Vegetarian', 
+  :description => 'The United Nations’ Food and Agriculture Organization estimates that the meat industry generates nearly one-fifth of the man-made greenhouse gas emissions that are accelerating climate change worldwide – more than the entire transportation sector! Skip steak once a week with your family, it would be the equivalent of taking your car off the road for nearly three months. Read more about Earth Day\'s <a href="http://www.earthday.org/takeaction/meatlessmonday_info.html">Meatless Monday</a>', 
+  :estimated_time => '30 Mins', 
+  :estimated_cost => 0, 
+  :location => 'Anywhere')
+Activity.create(
+  :title => 'Become an Organ Donor', 
+  :description => 'Around 1,700 people are on Australian organ transplant waiting lists at any one time. By choosing to record your donation decision on the Donor Register, you could save the lives of up to 10 people and improve the lives of many more. Become a donor <a href="http://www.humanservices.gov.au/customer/services/medicare/australian-organ-donor-register">here</a>. ', 
+  :estimated_time => '5 Minutes', 
+  :estimated_cost => 0, 
+  :location => 'Online')
+Activity.create(
+  :title => 'DIY "Clean Up Australia Day"', 
+  :description => 'In the tradition of "Clean Up Australia Day", consider going down to your local creek, park, beach, wherever, and doing a quick rubbish pick up. Be careful, wear gloves, and don\'t touch anything too dodgy.', 
+  :estimated_time => '30 Minutes', 
+  :estimated_cost => 0, 
+  :location => 'Various Locations', 
+  :estimated_cost => 0, 
+  :location => 'Various Locations')
+Activity.create(
+  :title => 'Help a Disadvantaged Woman Prepare for a Job Interview ', 
+  :description => '<a href="http://www.fittedforwork.org">Fitted for Work</a> runs a project to help dress disadvantaged women in an entire outfit that is professional, suitable for interview and carefully co-ordinated to make them look and feel good about themselves. Have a quick look in your closet to see if you have any clothes/handbags/accessories that would be suitable for this cause, and help support someone in their job seeking. Find out more about how to donate <a href="http://www.fittedforwork.org/index.php?option=com_content&view=article&id=125&Itemid=91#">here</a>.', 
+  :estimated_time => '30 Minutes', 
+  :estimated_cost => 0, 
+  :location => 'Anywhere')
+Activity.create(
+  :title => 'Knit or Sew to Save an Animal', 
+  :description => 'When juvenile kangaroos, wombats, gliders, wallabys, bandicoots or possums are injured and need to be taken in for care, they need to be kept warm and quiet. WIRES carers are always in need of suitable pouches. If you can\'t commit to animal rescue or care, maybe you can sew or knit to help a baby animal? See these links for instructions and where to send things for <a href="http://www.wires.org.au/images/stories/factsheetspdf/macropod_pouches.pdf">Kangaroos and Wallabies</a> or <a href="http://www.wires.org.au/images/stories/factsheetspdf/making_possum_pouches.pdf">Possums and Other Small Animals</a> ', 
+  :estimated_time => '1 Hour', 
+  :estimated_cost => 0, 
+  :location => 'Anywhere')
+Activity.create(
+  :title => 'Walk a Homeless Dog ', 
+  :description => 'At Monika\'s Doggie Rescue in Mona Vale you can walk a rescue dog, and give it some much needed human contact and exercise. For more details, go to their <a href="http://www.doggierescue.com/address.htm">website</a> or if you\'re not in this area look up your local dog shelter or RSPCA.', 
+  :estimated_time => '30 Minutes', 
+  :estimated_cost => 0, 
+  :location => 'Mona Vale and Others')
